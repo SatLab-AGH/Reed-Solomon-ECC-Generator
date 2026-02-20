c56ac220ea2d1bfa844b16c0053ca7089b23e827
Jan Rosa <jrosa@student.agh.edu.pl>
2026-02-20 15:59:26 +0000

    reformated mastrovitoGenerator

--- generators/MastrovitoVerilog.py
+++ generators/MastrovitoVerilog.py
@@ -1,6 +1,7 @@
 import logging
 from logging.handlers import RotatingFileHandler
-import unittest
+
+import pytest
 from generators import Mastrovito
 import json
 import datetime
@@ -24,8 +25,10 @@ class MastrovitoVerilogGenerator(Mastrovito.MastrovitoMatrixGenerator):
     def __init__(self):
         logger.info("Loading config")
         self._load_config()
+        degree = None
         if self.config["gf_degree"] is not None:
             degree = self.config["gf_degree"]
+        assert degree is not None
             
         irreducible_poly = None
         if len(self.config["irreducible_poly"]):
@@ -49,12 +52,12 @@ class MastrovitoVerilogGenerator(Mastrovito.MastrovitoMatrixGenerator):
         return \
             f'function automatic [{degree-1}:0]{self.func_name};\n' + \
             f'    input [{degree-1}:0] Z;\n' + \
-            f'    begin\n'
+             '    begin\n'
     
     def _generate_mult_function_foot(self):
         return \
-            f'    end\n' + \
-            f'endfunction\n\n'
+            '    end\n' + \
+            'endfunction\n\n'
                 
     def _generate_mult_function_body(self, mastrovito_matrix):
         output_str: str = str()
@@ -64,12 +67,12 @@ class MastrovitoVerilogGenerator(Mastrovito.MastrovitoMatrixGenerator):
             for col_indx, mastr_elem in enumerate(mastr_row):
                 if mastr_elem:
                     if len(output_line_body.strip()):
-                        output_line_body += f" ^ "
+                        output_line_body += " ^ "
                     output_line_body += f'Z[{col_indx}]'
                 else:
                     # Makes output xor'ing matrixlike alligned
                     output_line_body += f'  {" " * len(str(col_indx))} '
-                    output_line_body += f"   "
+                    output_line_body +=  "   "
                     
             output_str += output_line_head + output_line_body + ";\n"
             
@@ -78,7 +81,7 @@ class MastrovitoVerilogGenerator(Mastrovito.MastrovitoMatrixGenerator):
     def _generate_mult_function(self, A) -> str:
         
         mastrovito_matrix = self.get_mastrovito(A)
-        verilog_function_string: str = self._generate_mult_function_header(self.A, self.gf_degree)
+        verilog_function_string: str = self._generate_mult_function_header(A, self.gf_degree)
         verilog_function_string += self._generate_mult_function_body(mastrovito_matrix)
         verilog_function_string += self._generate_mult_function_foot()
         
@@ -88,13 +91,13 @@ class MastrovitoVerilogGenerator(Mastrovito.MastrovitoMatrixGenerator):
         return f'function automatic [{degree-1}:0]GF2_Deg{degree}_add;\n' + \
         f'    input [{degree-1}:0] add1;\n' + \
         f'    input [{degree-1}:0] add2;\n' + \
-        f'    begin\n' + \
-        f'        return add1^add2;\n' + \
-        f'    end\n' + \
-        f'endfunction\n\n'
+         '    begin\n' + \
+         '        return add1^add2;\n' + \
+         '    end\n' + \
+         'endfunction\n\n'
     
     def _generate_all_functions(self, multiplicants):
-        outstring = f'//Generated Functions\n\n'
+        outstring = '//Generated Functions\n\n'
         
         outstring += self._generate_add_function(self.gf_degree)
 
@@ -109,14 +112,14 @@ class MastrovitoVerilogGenerator(Mastrovito.MastrovitoMatrixGenerator):
         return \
         f'if(GF_CONST_MULT == {A}) begin \t\t\t: generate_block_mult_{A}\n' + \
         f'    assign P = GF2_Deg{degree}_const_mult_by_{A}(B);\n' + \
-        f'end else '
+         'end else '
 
     
     def _generate_sum_if(self, degree):
         return \
-        f'\n' + \
+         '\n' + \
         f'assign PS = GF2_Deg{degree}_add(P, C);\n' + \
-        f'\n'
+         '\n'
 
     # Module
 
@@ -124,44 +127,44 @@ class MastrovitoVerilogGenerator(Mastrovito.MastrovitoMatrixGenerator):
         logger.info("Generating module header")
         return \
             f'module {self.config["design_name"]}_Deg{self.config["gf_degree"]} #\n' + \
-            f'(\n' + \
-            f'    parameter GF_CONST_MULT = 1\n' + \
-            f')\n' + \
-            f'(\n' + \
+             '(\n' + \
+             '    parameter GF_CONST_MULT = 1\n' + \
+             ')\n' + \
+             '(\n' + \
             f'    input wire [{self.gf_degree-1}:0] B,\n' + \
             f'    input wire [{self.gf_degree-1}:0] C,\n' + \
             f'    output wire [{self.gf_degree-1}:0] PS\n' + \
-            f');\n' + \
-            f'// P = GF_CONST_MULT*B ' + \
-            f'// PS = P + C\n' + \
-            f'\n' + \
+             ');\n' + \
+             '// P = GF_CONST_MULT*B ' + \
+             '// PS = P + C\n' + \
+             '\n' + \
             f'wire [{self.gf_degree-1}:0]P;\n' + \
-            f'\n'
+             '\n'
 
     
     def _generate_module_foot(self):
         return \
-            f'\n' + \
-            f'endmodule;\n'
+            '\n' + \
+            'endmodule;\n'
     
     def _generate_module_body(self, multiplicants):
         logger.info("Generating module body")
         outstring = \
-            f'generate\n' + \
-            f'\n'
+            'generate\n' + \
+            '\n'
 
 
         for multiplicant in multiplicants:
             outstring += self._generate_mult_if(multiplicant, self.gf_degree)
         outstring += \
-            f'begin \t\t\t\t\t: generate_block_mult_INVALID\n' + \
-            f'    $fatal("Not generated Constant Multiplicant Selected: %d.", GF_CONST_MULT);\n' + \
-            f'end\n\n'
+            'begin \t\t\t\t\t: generate_block_mult_INVALID\n' + \
+            '    $fatal("Not generated Constant Multiplicant Selected: %d.", GF_CONST_MULT);\n' + \
+            'end\n\n'
 
         outstring += self._generate_sum_if(self.gf_degree)
 
         outstring += \
-            f'endgenerate\n'
+            'endgenerate\n'
 
         return outstring
 
@@ -184,15 +187,15 @@ class MastrovitoVerilogGenerator(Mastrovito.MastrovitoMatrixGenerator):
             f'// Project Name: {self.config["project_name"]}\n' + \
             f'// Description: {self.config["description"]}\n' + \
             f'// Dependencies: {self.config["dependencies"]}\n' + \
-            f'//\n' + \
-            f'// Design Specific Parameters: \n' + \
+             '//\n' + \
+             '// Design Specific Parameters: \n' + \
             f'// Irreducible Polynomial: {self.gf_field.irreducible_poly}\n' + \
             f'// Degree: {self.gf_degree}\n' + \
             f'// Constant Multiplicants: {self.config["constant_multplicants"]}\n' + \
             f'// Additional Comments: {self.config["additional_comments"]}\n' + \
             f'{"//" * 20} \n\r\n\r' + \
-            f'`timescale 1ns/1ps'
-    
+             '`timescale 1ns/1ps'
+
     def print_verilog_file(self):
         file_name = f'{self.config["design_name"]}_Deg{self.config["gf_degree"]}.v'
 
