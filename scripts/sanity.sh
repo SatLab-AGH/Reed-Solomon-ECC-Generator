6d89b91707f00d9d9771e891bbb975ece0543dae
Jan Rosa <jan.rosa02@gmail.com>
2026-03-04 17:08:11 +0000

    --amend

--- scripts/sanity.sh
+++ scripts/sanity.sh
@@ -1,13 +1,18 @@
-set -eu
+set -e
 
 source ./.venv/bin/activate
 
 cd src
 
 python3 -m generators.MastrovitoMatrix
+echo "Generated MastrovitoMatrix"
 python3 -m generators.MastrovitoVerilog
+echo "Generated MastrovitoVerilog"
 python3 -m generators.RSSegmentVerilog
+echo "Generated RSSegmentVerilog"
 python3 -m generators.RSAccumulatorVerilog
+echo "Generated RSAccumulatorVerilog"
 python3 -m generators.RSAXISVerilog
+echo "Generated RSAXISVerilog"
 
 cd -
